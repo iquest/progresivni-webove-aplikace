08b895266ecc92efc06a81a1e1e33faf6766d241
malkodima <malko@iquest.com>
2021-07-19 09:32:07 +0200

    aos mobile disable

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,56 @@
-$(function(){
-	//Scrollable links
-	$('a.scroll-link').click(function(){
-		var offset = -$('#main-nav').height();
-		$.scrollTo($(this).attr('href'), {
-			duration : 500,
-			offset: offset
-		});
-		return false;
-	});
+$(function () {
+  //Scrollable links
+  $("a.scroll-link").click(function () {
+    var offset = -$("#main-nav").height();
+    $.scrollTo($(this).attr("href"), {
+      duration: 500,
+      offset: offset,
+    });
+    return false;
+  });
 
-	//Scroll animation
-	AOS.init({
-		offset: 0,
-		duration: 800
-	});
+  //Scroll animation
+  AOS.init({
+    offset: 0,
+    duration: 800,
+    disable: "mobile",
+  });
 
-	//Misc
-	$('[data-copy-to]').each(function(){
-		$($(this).attr('data-copy-to')).html($(this).html());
-	});
+  //Misc
+  $("[data-copy-to]").each(function () {
+    $($(this).attr("data-copy-to")).html($(this).html());
+  });
 
-	//Phones
-	var zi = 500;
-	$('.header-phone-no').mouseenter(function(){
-		zi++;
-		var el = $('#' + $(this).attr('data-target'));
-		el.addClass('active');
-		el.find('.header-phone-pic').css({zIndex: zi});
-	}).mouseleave(function(){
-		$('#' + $(this).attr('data-target')).removeClass('active');
-	})
+  //Phones
+  var zi = 500;
+  $(".header-phone-no")
+    .mouseenter(function () {
+      zi++;
+      var el = $("#" + $(this).attr("data-target"));
+      el.addClass("active");
+      el.find(".header-phone-pic").css({ zIndex: zi });
+    })
+    .mouseleave(function () {
+      $("#" + $(this).attr("data-target")).removeClass("active");
+    });
 
-	function bsIs(env){
-		return $('#environment-' + env).is(':visible');
-	}
+  function bsIs(env) {
+    return $("#environment-" + env).is(":visible");
+  }
 });
 
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', function() {
-		navigator.serviceWorker.register('/sw.js').then(function(registration) {
-			console.log('ServiceWorker registration successful with scope: ', registration.scope);
-		}, function(err) {
-			console.log('ServiceWorker registration failed: ', err);
-
-		});
-	});
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/sw.js").then(
+      function (registration) {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
 }
